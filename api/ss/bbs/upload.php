819e6b8e216fed6df3cd672ed350b93c23d1c145
wish <wish@sek.ai>
2022-11-07 21:28:36 +1100

    rewrite bbs upload.php

--- api/ss/bbs/upload.php
+++ api/ss/bbs/upload.php
@@ -1,23 +1,26 @@
 <?php
 
-header('Content-Length: '.filesize('resp.txt'));
-header('Connection: close');
-header('Content-Type: text/xml; charset=UTF-8');
+if (!array_key_exists('img', $_FILES)) {
+	throw new Exception('No file');
+}
 
-$discord_webhook_url = "";
+$discord_webhook_url = '';
 
 $json_data = [
-    "content" => "",
-	"username" => "BBS Bot",
-    "file" => curl_file_create($_FILES["img"]["tmp_name"], $_FILES["img"]["type"], $_FILES["img"]["name"])
+    'content' => '',
+	'username' => 'BBS Bot',
+    'file' => curl_file_create($_FILES['img']['tmp_name'], $_FILES['img']['type'], $_FILES['img']['name'])
 ];
 
 $curl = curl_init($discord_webhook_url);
-curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
 curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
-
 curl_exec($curl);
 curl_close($curl);
+
+header('Content-Length: '.filesize('resp.txt'));
+header('Connection: close');
+header('Content-Type: text/xml; charset=UTF-8');
 
 readfile('resp.txt');
