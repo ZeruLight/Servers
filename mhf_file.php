56ed5f8f8fa2e6a77f442530cde895ab8b3f18c6
wish <wish@sek.ai>
2023-04-16 18:35:16 +1000

    fix content-length header

--- mhf_file.php
+++ mhf_file.php
@@ -6,7 +6,7 @@ if (isset($_GET['key'])) {
 		$platform = "51";
 	}
 	header(sprintf('Content-Disposition: inline; filename="MHFUP_%s.DAT"', $platform));
-	header('Content-Length: '.filesize('key.txt'));
+	header('Content-Length: '.filesize(sprintf('key%s.txt', $platform)));
 	header('Connection: close');
 	header('Content-Type: application/octet-stream');
 	readfile(sprintf('key%s.txt', $platform));
