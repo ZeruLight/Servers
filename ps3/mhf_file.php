1ae2ae35474390dc7de007552dd31c8b6b078f44
wish <wish@sek.ai>
2024-03-09 14:58:02 +1100

    remove PS3 Patch Server default

--- ps3/mhf_file.php
+++ ps3/mhf_file.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_GET['key'])) {
-	header('Content-Disposition: inline; filename="MHFUP_00.DAT"');
+	header('Content-Disposition: inline; filename="MHFUP_51.DAT"');
 	header('Content-Length: ' . filesize('key.txt'));
 	header('Connection: close');
 	header('Content-Type: application/octet-stream');
